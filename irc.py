cefabf5695998a35e3b18695ada33186855e563f
RomEz10 <rom.ezry@gmail.com>
2020-02-29 20:03:58 +0200

    this irc client is a bit specific to twitch.tv NICK + PASS are sent together because twitch doesn't open a connection when they're in different packets. irc client kinda working but reconnection not implemented can send whispers client is based on https://pythonspot.com/building-an-irc-bot/

---
+++ irc.py
@@ -0,0 +1,35 @@
+import socket
+
+encoding = 'utf-8'
+LINE_ENDINGS = '\r\n'    # This is appended to all messages sent by the socket (should always be CRLF)
+
+
+class IRC:
+    irc = socket.socket()
+    def __init__(self):
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, chan, msg):
+        fullmsg = "PRIVMSG" + " #" + chan + " :" + msg + LINE_ENDINGS
+        print(fullmsg)
+        self.irc.send(fullmsg.encode(encoding))
+
+    def sendWhisper(self, chan, target, msg):
+        fullmsg = "PRIVMSG" + " #" + chan + " :/w " + target + ' ' + msg + LINE_ENDINGS
+        print(fullmsg)
+        self.irc.send(fullmsg.encode(encoding))
+
+    def connect(self, server, channel, botnick, auth):
+        # defines the socket
+        print("connecting to:" + server)
+        self.irc.connect((server, 6667))  # connects to the server
+        self.irc.send(("PASS " + auth + LINE_ENDINGS + "NICK " + botnick + LINE_ENDINGS ).encode(encoding))
+        self.irc.send(("JOIN #" + channel + LINE_ENDINGS).encode(encoding))  # join the chan
+
+    def get_text(self):
+        text = self.irc.recv(4096)  # receive the text
+
+        if str(text).find('PING') != -1:
+            self.irc.send(('PONG ' + str(text).split()[1] + 'rn').encode(encoding))
+
+        return text
