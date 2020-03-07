34bbe39b91f99b91a8d4c9cf5707adf50879e2ce
RomEz10 <rom.ezry@gmail.com>
2020-03-08 00:41:27 +0200

    draw game now works, it ends after 30 secs timeout or someone guesses the right answer with '!draw <guess>' interfacing with database authorization code added and bot is now able to get twitch user id in order to have a unique key for db

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
 from irc import *
+import requests
+from authorization import authorize
+import draw_game
+import threading
+import databse
+from chatter import Chatter
+import creds
+
+draw_game = draw_game.DrawGame()
+draw_timer = threading.Timer(30.0, draw_game.timer)  # thread that limits the draw game to 30 seconds by calling timer
 
 
 def parseCommand(msg):
     # separate command from additional argument
-    print('space is found in ' + str(msg.find(' ')))
+    print('space is found in ' + str(msg.find(' ')))  # DOES NOT SUPPORT TWO ARG RIGHT NOW
     if msg.find(' ') != -1:  # check if there are args
         command = msg[1:msg.find(' ')]  # splice the ! and the arg after the command
         arguments = msg[msg.find(' ') + 1:]  # get arg - arg is the word after the command
@@ -13,7 +23,17 @@ def parseCommand(msg):
     print('arg: ' + arguments + ' command: ' + command)
     return command, arguments
 
-def draw():
+
+def draw(arg):
+    if draw_game.on_going is True:  # if a game is currently running
+        if draw_game.answer(arg) is True:
+            draw_timer.cancel()  # if answer was right cancel the timer and end the game
+    if arg == 'start':  # command argument was start to initiate a game
+        draw_game.on_going = True
+        draw_game.start_game()  # generate draw object
+        draw_timer.start()  # start the timeout timer
+    print(draw_game.on_going)
+    print(draw_game.draw)
     return 'draw'
 
 def exeCommand(command):
@@ -22,23 +42,37 @@ def exeCommand(command):
         'draw': draw
     }
     commanda = commands.get(command[0])
-    print(commanda())
+    print(commanda(command[1]))
 
-channel = ''
+
 server = 'irc.chat.twitch.tv'
-nickname = ''
-auth = ''
+
 irc = IRC()
+channel = creds.channel
+nickname = creds.nickname
+auth = creds.auth
 irc.connect(server, channel, nickname, auth)
-irc.send(channel, 'connected FeelsOkayMan')
+irc.send(creds.channel, 'connected FeelsOkayMan')
+access_token = authorize()  # add a function to count the time to refresh token
+db = databse.database
+
 while 1:
     text = irc.get_text().decode('utf-8')
-    print(text)
-    msg = text[text.rfind(':')+1:-2] #parse the IRC string to get the message '-2' removes the \r\n in the end of a message
-    print(msg)
-    if  msg[:1] == '!': #if first char is !- than its a command
+    print('raw: ' + text)
+    msg = text[text.rfind(':')+1:-2]  # parse the IRC string to get the message -2 removes the \r\n in the end of a msg
+    username = text[text.find(':')+1:text.find('!')]
+    print(msg + ' sent by ' + username)
+    response = requests.get('https://api.twitch.tv/helix/users?login=' + username,
+                            headers={'Authorization': 'Bearer ' + access_token[0]}).json()
+    print(response)
+    user_id = response.get('data', 'none')
+    if user_id != 'none':
+        user_id = user_id[0].get('id', 'none')  # response is JSON with a list (with one item) inside 'data' var
+    print('userid is: ' + str(user_id))
+    chatter = Chatter(twitch_id=user_id, name=username, points=0, guesses=0)
+    db.session.add(chatter)  # add a chatter to the database- only POC for now, gonna add chatters only if they get the correct answer
+    db.session.commit()
+    if msg[:1] == '!':  # if first char is !- than its a command
         command = parseCommand(msg)
         exeCommand(command)
 
-    if "PRIVMSG" in text and channel in text and "hello" in text:
-        irc.send(channel, "Hello!")
