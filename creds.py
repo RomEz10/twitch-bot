965a2e856ca48e102ac606ff5e94c5888fcacaf7
RomEz10 <rom.ezry@gmail.com>
2020-03-08 01:03:25 +0200

    cred template that other classes inherit from

---
+++ creds.py
@@ -0,0 +1,8 @@
+# for bot.py
+channel =  # channel to join
+nickname =  # bot nickname
+auth =  # oauth token from https://twitchapps.com/tmi/
+
+# for authorization.py
+clientid =  # client id from https://dev.twitch.tv/console/apps
+client_secret =  # client secret from https://dev.twitch.tv/console/apps
