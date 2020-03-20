ba5f18b450faf3f46cde6a4de2f922752cbfbda2
RomEz10 <rom.ezry@gmail.com>
2020-03-20 12:56:51 +0200

    Can now add points and guesses. also implemented in draw_game.

--- databse.py
+++ databse.py
@@ -4,15 +4,26 @@ import chatter
 
 
 # orm implementation of sqlalchemy
-class database:
+class DataBase:
     engine = create_engine('sqlite:///db.db', echo=True)  # creating a local sqlite db with debugging enabled
     chatter.Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
 
+    def exists(self, chat):
+        return str(self.session.query(chatter.Chatter).filter_by(twitch_id=chat.twitch_id).first()) != 'None'
+
     def add(self, chat):
-        exists = str(self.session.query(chatter.Chatter).filter_by(name=chat.id).first()) != 'None'
-        if exists:
+        if not self.exists(self, chat):
+            # make sure to add a user doesn't exist in the database before you try to add him
             self.session.add(chat)
             self.session.commit()
+
+    def add_points(self, chat, amount):
+        self.session.query(chatter.Chatter).filter_by(twitch_id=chat.twitch_id).first().points += amount
+        self.session.commit()
+
+    def add_guesses(self, chat):
+        self.session.query(chatter.Chatter).filter_by(twitch_id=chat.twitch_id).first().guesses += 1
+        self.session.commit()
 
 
