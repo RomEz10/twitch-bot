c1cf02239f58ee0fcbf9c4886b07d00cbf6dc736
RomEz10 <rom.ezry@gmail.com>
2020-04-05 03:01:54 +0300

    new creds required, should be generated using web_server.py
    Signed-off-by: RomEz10 <rom.ezry@gmail.com>

--- creds.py
+++ creds.py
@@ -6,3 +6,7 @@ auth =  # oauth token from https://twitchapps.com/tmi/
 # for authorization.py
 clientid =  # client id from https://dev.twitch.tv/console/apps
 client_secret =  # client secret from https://dev.twitch.tv/console/apps
+
+# for whispers.py
+whispers_token =  # used to authenticate in websocket with whispers:read scope
+refresh_token =  # used to get a new token after expiration
