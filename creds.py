7cd06251e9678388950f8e97eda7e0216f757f77
RomEz10 <rom.ezry@gmail.com>
2020-05-24 20:46:25 +0300

    vars name changed and refresh_timer and scopes added (no need to change them tbh, they're not actually required)
    Signed-off-by: RomEz10 <rom.ezry@gmail.com>

--- creds.py
+++ creds.py
@@ -4,9 +4,11 @@ nickname =  # bot nickname
 auth =  # oauth token from https://twitchapps.com/tmi/
 
 # for authorization.py
-clientid =  # client id from https://dev.twitch.tv/console/apps
+client_id =  # client id from https://dev.twitch.tv/console/apps
 client_secret =  # client secret from https://dev.twitch.tv/console/apps
 
 # for whispers.py
-whispers_token =  # used to authenticate in websocket with whispers:read scope
+oauth_user_token =  # used to authenticate with pubsub
 refresh_token =  # used to get a new token after expiration
+refresh_timer = 300
+scopes = []
