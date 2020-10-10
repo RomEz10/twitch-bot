a2973e07dae5a4ea90562ec213040ee2f69ec72f
RomEz10 <rom.ezry@gmail.com>
2020-10-10 19:22:20 +0300

    small change, checking if arg is not null before accessing it.
    Signed-off-by: Rom <RomEz10@gmail.com>

--- draw_game.py
+++ draw_game.py
@@ -13,18 +13,19 @@ class DrawGame(base_game.BaseGame):
         super().__init__(choices, time, irc)
 
     async def game_command(self, arg, username, db, chat):
-        if arg[0] == 'wins':
-            await self.irc.send(self.irc.channel, username + ', you have ' +
-                                str(db.get_guesses(db, chat)) + ' wins')
-        else:
-            if self.on_going is True:  # if a game is currently running
-                if await self.check_answer(arg[0], username, db, chat) is True:
-                    self.timer.cancel()  # if answer was right cancel the timer and end the game
+        if arg:
+            if arg[0] == 'wins':
+                await self.irc.send(self.irc.channel, username + ', you have ' +
+                                    str(db.get_guesses(db, chat)) + ' wins')
             else:
-                if arg[0] == 'start':  # command argument was start to initiate a game
-                    await self.start_game('Drawing game has started! you have a minute to guess the right answer!')
-        print(self.on_going)
-        return 'draw'
+                if self.on_going is True:  # if a game is currently running
+                    if await self.check_answer(arg[0], username, db, chat) is True:
+                        self.timer.cancel()  # if answer was right cancel the timer and end the game
+                else:
+                    if arg[0] == 'start':  # command argument was start to initiate a game
+                        await self.start_game('Drawing game has started! you have a minute to guess the right answer!')
+            print(self.on_going)
+            return 'draw'
 
     async def start_game(self, opening_msg):  # start the game and pick a random object to draw
         self.timer = threading.Timer(60.0,
